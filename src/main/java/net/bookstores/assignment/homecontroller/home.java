4dd4c28519cf3a2076677b588a80401cc5488dc3
nguyenchinguyen1 <nguyenncpd10775@gmail.com>
2025-03-23 22:13:56 +0700

    nguyensua

--- src/main/java/net/bookstores/assignment/homecontroller/home.java
+++ src/main/java/net/bookstores/assignment/homecontroller/home.java
@@ -1,39 +1,26 @@
 package net.bookstores.assignment.homecontroller;
 
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import net.bookstores.assignment.dao.AuthorDao;
-import net.bookstores.assignment.dao.BookDao;
-import net.bookstores.assignment.entities.Author;
-import net.bookstores.assignment.entities.Book;
+import net.bookstores.assignment.service.HomeService;
 
 @Controller
-public class home {
+public class Home {
+
     @Autowired
-    BookDao bookDao;
-    @Autowired
-    AuthorDao authorDao;
+    private HomeService homeService;
 
     @GetMapping("/")
     public String index(Model model) {
-        Pageable topFive = PageRequest.of(0, 5);
-        List<Book> top5 = bookDao.findTop5Books(topFive);
-        model.addAttribute("top5", top5);
-
-        Pageable topTen = PageRequest.of(0, 10);
-        List<Book> top10 = bookDao.find10RandomBooks(topTen);
-        model.addAttribute("top10", top10);
-
-        Pageable topFiveAuthor = PageRequest.of(0, 5);
-        List<Author> top5Author = authorDao.findRandom5Authors(topFiveAuthor);
-        model.addAttribute("top5Author", top5Author);
+        model.addAttribute("top5", homeService.getTop5Books());
+        model.addAttribute("top10", homeService.getTop10RandomBooks());
+        model.addAttribute("top5Author", homeService.getTop5RandomAuthors());
         return "/home/index";
     }
 
---
+++ src/main/java/net/bookstores/assignment/service/HomeService.java
@@ -0,0 +1,40 @@
+package net.bookstores.assignment.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Pageable;
+
+import net.bookstores.assignment.dao.AuthorDao;
+import net.bookstores.assignment.dao.BookDao;
+import net.bookstores.assignment.entities.Author;
+import net.bookstores.assignment.entities.Book;
+
+@Service
+    public class HomeService {
+    
+        @Autowired
+        private BookDao bookDao;
+    
+        @Autowired
+        private AuthorDao authorDao;
+    
+        public List<Book> getTop5Books() {
+            Pageable topFive = PageRequest.of(0, 5);
+            return bookDao.findTop5Books(topFive);
+        }
+    
+        public List<Book> getTop10RandomBooks() {
+            Pageable topTen = PageRequest.of(0, 10);
+            return bookDao.find10RandomBooks(topTen);
+        }
+    
+        public List<Author> getTop5RandomAuthors() {
+            Pageable topFiveAuthor = PageRequest.of(0, 5);
+            return authorDao.findRandom5Authors(topFiveAuthor);
+        }
+    }
+    
+
