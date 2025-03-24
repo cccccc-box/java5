ffa9b8d9696435b02160cc88f3a88ffa6b2917c2
cccccc-box <monga.monga.monga.monga.nonga@gmail.com>
2025-03-24 08:13:45 +0700

    update ten file

--- src/main/java/net/bookstores/assignment/homecontroller/Home_tmp.java
+++ src/main/java/net/bookstores/assignment/homecontroller/Home_tmp.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import net.bookstores.assignment.service.HomeService;
 
 @Controller
-public class Home {
+public class Home_tmp {
 
     @Autowired
     private HomeService homeService;
