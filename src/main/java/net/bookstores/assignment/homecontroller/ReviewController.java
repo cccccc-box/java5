068f83abe451c3e10b4955b443ddc023ea9fa64e
tranconghoc <hoctran381@gmail.com>
2025-03-31 01:18:24 +0700

    Cập nhật ReviewService và thêm ReviewController

---
+++ src/main/java/net/bookstores/assignment/homecontroller/ReviewController.java
@@ -0,0 +1,43 @@
+package net.bookstores.assignment.homecontroller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.bookstores.assignment.entities.Review;
+import net.bookstores.assignment.service.ReviewService;
+
+@RestController
+@RequestMapping("/reviews")
+public class ReviewController {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping
+    public Review createReview(@RequestBody Review review) {
+        return reviewService.saveReview(review);
+    }
+
+    @GetMapping
+    public List<Review> getAllReviews() {
+        return reviewService.getAllReviews();
+    }
+
+    @GetMapping("/{id}")
+    public Review getReviewById(@PathVariable Integer id) {
+        return reviewService.getReviewById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteReview(@PathVariable Integer id) {
+        reviewService.deleteReview(id);
+    }
+}
--- src/main/java/net/bookstores/assignment/service/ReviewService.java
+++ src/main/java/net/bookstores/assignment/service/ReviewService.java
@@ -1,5 +1,37 @@
 package net.bookstores.assignment.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.bookstores.assignment.dao.ReviewDao;
+import net.bookstores.assignment.entities.Review;
+
+@Service
 public class ReviewService {
 
+    @Autowired
+    private ReviewDao reviewDao;
+
+    // Lưu đánh giá mới
+    public Review saveReview(Review review) {
+        return reviewDao.save(review);
+    }
+
+    // Lấy tất cả đánh giá
+    public List<Review> getAllReviews() {
+        return reviewDao.findAll();
+    }
+
+    // Tìm kiếm đánh giá theo ID
+    public Optional<Review> getReviewById(Integer id) {
+        return reviewDao.findById(id);
+    }
+
+    // Xóa đánh giá
+    public void deleteReview(Integer id) {
+        reviewDao.deleteById(id);
+    }
 }
