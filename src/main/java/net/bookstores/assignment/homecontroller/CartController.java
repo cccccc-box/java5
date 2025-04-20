789236e4ad6fddcb2401d39f9bd1e63421d8d080
nguyenchinguyen1 <nguyenncpd10775@gmail.com>
2025-04-20 21:24:06 +0700

    nguyen

--- src/main/java/net/bookstores/assignment/homecontroller/CartController.java
+++ src/main/java/net/bookstores/assignment/homecontroller/CartController.java
@@ -1,27 +1,28 @@
-package net.bookstores.assignment.homecontroller;
+// package net.bookstores.assignment.homecontroller;
 
 
-import java.util.Optional;
+// import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.*;
 
-import net.bookstores.assignment.entities.Book;
-import net.bookstores.assignment.entities.User;
-import net.bookstores.assignment.service.BookService;
-import net.bookstores.assignment.service.CartService;
-import net.bookstores.assignment.service.UserService;
+// import net.bookstores.assignment.entities.Book;
+// import net.bookstores.assignment.entities.User;
+// import net.bookstores.assignment.service.BookService;
+// import net.bookstores.assignment.service.CartService;
+// import net.bookstores.assignment.service.UserService;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class CartController {
-    @GetMapping("/cart")
-    public String cart() {
-        return "home/cart";
->>>>>>> f9372da62a339d8a30cfe73c019da87719361912
-    }
-}
+// @Controller
+// public class CartController {
+//     @GetMapping("/cart")
+//     public String cart() {
+//         return "home/cart";
+//         //hjf
+// >>>>>>> f9372da62a339d8a30cfe73c019da87719361912
+//     }
+// }
